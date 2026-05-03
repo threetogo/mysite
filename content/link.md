87159855f56a764f72a05b383c124268546b03d8
threetogo <mi7a920@gmail.com>
2026-05-03 14:35:08 +0900

    デプロイ設定を更新

---
+++ content/link.md
@@ -0,0 +1,14 @@
++++
+
+title = "link"
+
++++
+
+### リンクについて
+
+誘爆<br>http～～<br>個人サイト・同人サイトからのみリンクフリー<br>同好の方による好意からの紹介・引用目的以外での使用はできません
+
+### ブックマーク
+
+\- \[ロリポップ！レンタルサーバー](https://lolipop.jp/)<br>- \[HUGO](https://gohugo.io/)<br>- \[Git Hub](https://github.com/)<br>- \[unsplash](https://unsplash.com/ja)<br>
+
---
+++ get-content.js
@@ -0,0 +1,27 @@
+﻿const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const API_KEY = process.env.MICROCMS_API_KEY;
+const options = {
+  hostname: 't9d264vpqv.microcms.io',
+  path: '/api/v1/diary?limit=100',
+  headers: { 'X-MICROCMS-API-KEY': API_KEY }
+};
+https.get(options, function(res) {
+  var data = '';
+  res.on('data', function(c) { data += c; });
+  res.on('end', function() {
+    var posts = JSON.parse(data).contents;
+    var dir = path.join('content', 'posts');
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+    posts.forEach(function(post) {
+      var date = new Date(post.publishedAt || post.createdAt).toISOString();
+      var body = post.body || '';
+      var title = post.title.replace(/"/g, '\\"');
+      var content = '+++\ntitle = "' + title + '"\ndate = "' + date + '"\ndraft = false\n+++\n\n' + body + '\n';
+      fs.writeFileSync(path.join(dir, post.id + '.md'), content, 'utf8');
+      console.log('完了: ' + post.title);
+    });
+    console.log('合計 ' + posts.length + ' 件');
+  });
+});

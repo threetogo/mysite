d5bd7b7cb4946a87e55da767c7e2c1da2706392a
threetogo <mi7a920@gmail.com>
2026-05-14 17:39:34 +0900

    feat: タグバッジスタイル追加・文字化け修正

--- get-content.js
+++ get-content.js
@@ -4,10 +4,11 @@ const path = require('path');
 const API_KEY = process.env.MICROCMS_API_KEY;
 const options = {
   hostname: 't9d264vpqv.microcms.io',
-  path: '/api/v1/diary?limit=100',
+  path: '/api/v1/diary?limit=100&depth=2',
   headers: { 'X-MICROCMS-API-KEY': API_KEY }
 };
 https.get(options, function(res) {
+  res.setEncoding('utf8');
   var data = '';
   res.on('data', function(c) { data += c; });
   res.on('end', function() {
@@ -18,7 +19,12 @@ https.get(options, function(res) {
       var date = new Date(post.publishedAt || post.createdAt).toISOString();
       var body = post.body || '';
       var title = post.title.replace(/"/g, '\\"');
-      var content = '+++\ntitle = "' + title + '"\ndate = "' + date + '"\ndraft = false\n+++\n\n' + body + '\n';
+      var tags = '';
+      if (post.tags && post.tags.length > 0) {
+        var tagNames = post.tags.map(function(t) { return '"' + t.tags + '"'; });
+        tags = 'tags = [' + tagNames.join(', ') + ']\n';
+      }
+      var content = '+++\ntitle = "' + title + '"\ndate = "' + date + '"\ndraft = false\n' + tags + '+++\n\n' + body + '\n';
       fs.writeFileSync(path.join(dir, post.id + '.md'), content, 'utf8');
       console.log('完了: ' + post.title);
     });
